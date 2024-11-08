390d59aedf0611d18caea924507b52c8421c1ce2
Juliette <julchristley@gmail.com>
2024-11-07 21:53:18 -0700

    grouping done

--- autogrouptool.py
+++ autogrouptool.py
@@ -3,14 +3,16 @@ import maya.cmds as cmds
 objects = cmds.ls(selection=True)
 
 for objs in objects:
+    # querying
     position = cmds.xform(objs, query=True, worldSpace=True, translation=True)
     rotation = cmds.xform(objs, query=True, worldSpace=True, rotation=True)
 
-    gname = objs + '_Grp'
-    group = cmds.group(empty=True, name=gname)
+    #creating group, renaming it
+    group = cmds.group(empty=True, name=objs + '_Grp')
+    
+    # matching group to object rotation/location
     cmds.xform(group, worldSpace=True, translation=position)
     cmds.xform(group, worldSpace=True, rotation=rotation)
 
+    #parenting object to group
     cmds.parent(objs, group)
-
-    cmds.makeIdentity(objs, apply=True, translate=True, rotate=True, scale=True, normal=False)
